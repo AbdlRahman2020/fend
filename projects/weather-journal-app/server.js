e0741e059cf1259a19389750e61261cba3788c09
AbdlRahman Essa <abdlrahmanfathy2020@gmail.com>
2021-03-13 22:19:44 +0100

    init

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -1,19 +1,52 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+let projectData = [];
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
+const bodyParser = require('body-parser')
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 8000;
+const server = app.listen(port, listening);
+function listening(){
+  console.log("server running"); 
+  console.log(`running on localhost: ${port}`);
+}
+
+
+//Add a GET route that returns the projectData object in your server code
+app.get('/all', sendData);
+function sendData (request, response) {
+  response.send(projectData);
+};
+
+
+// POST route that adds incoming data to projectData
+app.post('/add', addData);
+function addData(req, res)
+{
+  const newEntry = {
+    temperature: req.body.temp,
+    date: req.body.date,
+    userResponse: req.body.userResponse
+  }
+
+  projectData.push(newEntry);
+  res.send(projectData);
+}
--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,5 +1,106 @@
 /* Global Variables */
-
+// Personal API Key for OpenWeatherMap API
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip='
+const apiKey = '&appid=5b1d870d14c6880b478ff18bd3ef33e9';
+let zipCode = document.getElementById('zip').value;
+console.log(zipCode);
+let feelings = document.getElementById('feelings').value;
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+
+
+// Event listener to add function to existing HTML DOM element
+document.getElementById('generate').addEventListener('click', performAction);
+
+
+
+
+/* Function to GET Web API Data*/
+const getWeather = async () => {
+  zipCode = document.getElementById('zip').value;
+  feelings = document.getElementById('feelings').value;
+
+  const req = await fetch(baseURL + zipCode + apiKey)
+  try {
+
+    const data = await req.json();
+    //console.log(data)
+    return data;
+
+  }
+
+  catch (error) {
+    console.log("error", error);
+    // appropriately handle the error
+  }
+}
+
+/* Function called by event listener */
+function performAction(e) {
+  if (zip.value === "") {
+    alert('Please enter a valid Zip Code');
+  } else {
+    getWeather()
+      .then(function (allData) {
+        postData('/add', { temp: allData.main.temp, date: newDate, userResponse: feelings })
+      })
+      .then(function () {
+        retrieveData('/all');
+      })
+  }
+}
+
+
+/* Function to POST data */
+const postData = async (url = '', data = {}) => {
+  //console.log(data);
+  const response = await fetch(url, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    // Body data type must match "Content-Type" header        
+    body: JSON.stringify(data),
+  });
+
+  try {
+    const newData = await response.json();
+    console.log(newData);
+    return newData;
+  } catch (error) {
+    console.log("error", error);
+  }
+}
+
+
+
+/* Function to GET Project Data */
+const retrieveData = async (url) => {
+  
+  const req = await fetch(url); 
+
+  try {
+    const newData = await req.json();
+    let len = newData.length;
+    
+    const newEntry = {
+      temperaturess: newData[len - 1].temperature,
+      datess: newData[len - 1].date,
+      userResponsess: newData[len - 1].userResponse
+    }
+    console.log(newData[len - 1]);
+    let displayDate = document.getElementById('date');
+    let  displayTemp = document.getElementById('temp');
+    let  displayFeelings = document.getElementById('content');
+    
+    displayDate.innerHTML = "Date: " + newEntry.datess;
+    displayTemp.innerHTML = "Temperature: " + newEntry.temperaturess;
+    displayFeelings.innerHTML = "Feelings: " + newEntry.userResponsess;
+
+  } catch (error) {
+    console.log("error", error);
+  }
+}
